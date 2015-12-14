3aae249a4d5467875375690c30c2921c1650f53f
Benji Dalton <daylightsavings@gmail.com>
2015-12-14 23:48:43 +1300

    add category reference with page

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
   has_many :categorizations
   has_many :entries, through: :categorizations
+  has_many :pages, through: :categorizations  
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -5,4 +5,19 @@ class Page < ActiveRecord::Base
   has_many :tags, through: :taggings
   has_many :categorizations
   has_many :categories, through: :categorizations
+
+  def self.tagged_with(topic)
+    tags = Tag.find_by(topic: topic) || Tag.new(topic: topic)
+    tags.entries
+  end
+
+  def all_tags=(topics)
+    self.tags = topics.downcase.split(/[\s,]+/).map do |topic|
+        Tag.where(topic: topic.strip).first_or_create!
+    end
+  end
+
+  def all_tags
+    self.tags.map(&:topic).join(", ")
+  end
 end
