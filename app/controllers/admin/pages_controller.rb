12a685bc014a3ce61fa767762edb058e9a3df7ab
Benji Dalton <daylightsavings@gmail.com>
2015-12-21 01:38:21 +1300

    changes pages before action to clearance helper rather than custom redirect

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,5 +1,5 @@
 class Admin::PagesController < ApplicationController
-  before_action :authorize_admin
+  before_action :require_login
   before_action :set_page, only: [:show, :edit, :update, :destroy]
 
   def index
