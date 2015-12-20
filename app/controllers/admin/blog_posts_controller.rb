c149258c702d24b83b8bbaf371c4732342386bed
Benji Dalton <daylightsavings@gmail.com>
2015-12-21 02:30:53 +1300

    fixes blog post admin views

--- app/controllers/admin/blog_posts_controller.rb
+++ app/controllers/admin/blog_posts_controller.rb
@@ -1,44 +1,46 @@
 class Admin::BlogPostsController < ApplicationController
   before_action :require_login
-  respond_to :html, :js
+  before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @blog_posts = BlogPost.all
   end
 
   def show
-    @blog_post = BlogPost.find(params[:id])
+  end
+
+  def new
+    @blog_post = BlogPost.new
   end
 
   def create
     @blog_post = BlogPost.create!(blog_params)
     flash[:notice] = "Blog Post Created"
-    respond_with do |format|
-      format.html { redirect_to admin_blog_posts_url }
-    end
+    redirect_to admin_blog_posts_url
   end
 
   def edit
-    @blog_post = BlogPost.find(params[:id])
-    @blog_post.update!(blog_params)
   end
 
   def update
-    if @page.update(page_params)
+    if @blog_post.update(blog_params)
       flash[:notice] = "Blog Post Updated"
       redirect_to admin_blog_posts_url
     end
   end
 
   def destroy
-    @blog_post = BlogPost.find(params[:id])
     @blog_post.destroy!
-    flash[:notice] = "Blog Post Deleted"    
+    flash[:notice] = "Blog Post Deleted"
     redirect_to admin_blog_posts_url
   end
 
   private
-  def blog_params
-    params.require(:blog_post).permit(:title, :body, :status, :feature_image, :author, :all_tags)
-  end
+    def set_blog_post
+      @blog_post = BlogPost.find(params[:id])
+    end
+
+    def blog_params
+      params.require(:blog_post).permit(:title, :body, :status, :feature_image, :author, :all_tags)
+    end
 end
