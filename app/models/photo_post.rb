cde2cf60665c66fe18f69252aff1adb1468dda56
Benji Dalton <daylightsavings@gmail.com>
2015-12-29 02:11:32 +1300

    adds image processing to photoPost

--- app/models/photo_post.rb
+++ app/models/photo_post.rb
@@ -1,3 +1,4 @@
 class PhotoPost < ActiveRecord::Base
-  belongs_to :gallery  
+  belongs_to :gallery
+  mount_uploader :image, ImageUploader  
 end
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -27,6 +27,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
+  # process :resize_to_fill => [350, 350]
   process :resize_to_fill => [350, 350]
   #
   # def scale(width, height)
