c27d37b41ec5059fdb75defc037c7229dd68b105
Benji Dalton <daylightsavings@gmail.com>
2015-12-29 00:51:29 +1300

    adds some crud to admin users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,23 @@
 class Admin::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :authorize_admin
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @user.update!(user_params)
+    redirect_to admin_user_path(@user)
+  end
 
   private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
 		def user_params
 			require(:user).permit(:avatar)
 		end
