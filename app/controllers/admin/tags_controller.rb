9e02cdf785a9aad95596755279d4162e18eeaa12
Benji Dalton <daylightsavings@gmail.com>
2015-12-30 00:15:15 +1300

    adds tag views and link to admin dashboard

---
+++ app/controllers/admin/tags_controller.rb
@@ -0,0 +1,47 @@
+class Admin::TagsController < ApplicationController
+  before_action :authorize_user
+  before_action :set_tag, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tags = Tag.all
+  end
+
+  def show
+  end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    @tag = Tag.create!(tag_params)
+    flash[:notice] = "Tag Created"
+    redirect_to admin_tags_url
+  end
+
+  def edit
+  end
+
+  def update
+    @tag.update!(tag_params)
+    flash[:notice] = "Tag Updated"
+    redirect_to admin_tags_url
+  end
+
+  def destroy
+    @tag.destroy!
+    flash[:notice] = "Tag Destroyed"
+    redirect_to admin_tags_url
+  end
+
+  private
+
+    def set_tag
+      @tag = Tag.find(params[:id])
+    end
+
+    def tag_params
+      params.require(:tag).permit(:topic)
+    end
+
+end
