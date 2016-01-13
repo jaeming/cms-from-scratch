05df5149eaceb0b29454a5de96b76b5101c34723
Benji Dalton <daylightsavings@gmail.com>
2016-01-13 18:50:25 +1300

    adds basic admin/navigation functionality

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -1,20 +1,25 @@
-class SettingsController < ApplicationController
+class Admin::SettingsController < ApplicationController
   before_filter :authorize_admin
   before_filter :find_or_initilize_settings
 
   def index
+    @navigations = Navigation.all
   end
 
   def show
   end
 
+  def edit
+  end
+
   def update
     @setting.update!(setting_params)
+    redirect_to admin_settings_url
   end
 
   private
   def setting_params
-    params.require(:setting).permit(:comments_approval, :comments_only_registered, :new_user_signups, :site_title, :site_description, :timezone)
+    params.require(:setting).permit(:site_title, :site_description)
   end
 
   def find_or_initilize_settings
