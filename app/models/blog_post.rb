df6121edb2eee7920d701ab70a5b21deb94780e5
Benji Dalton <daylightsavings@gmail.com>
2016-02-01 03:29:26 -0600

    automatically sets author column on blog posts after create

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -2,13 +2,15 @@ class BlogPost < ActiveRecord::Base
   belongs_to :user
   has_many :taggings
   has_many :tags, through: :taggings
+  after_create :set_author
   include Taggings
 
   scope :published, -> { where(draft: false) }
   scope :drafts, -> { where(draft: true) }
 
-  def author
-    self.user.nickname
+  def set_author
+    self.author = self.user.nickname
+    self.save
   end
 
 end
